6d2fcf9dfa2d7f99b13653eaf67d1a199cd24c41
Akshay Kumar Singh <72185763+07akshay@users.noreply.github.com>
2021-04-27 22:08:23 +0530

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import streamlit as st
+import pandas as pd
+
+# In[9]:
+
+
+st.write("""
+# This is the webpage of AKSHAY
+
+## lets build better
+also this is cool mate\n
+rahul is a gandu
+""")
+st.sidebar.header("yeah you got it mate")
+c = 67*78
+st.write("the result is " +str(c))
+# In[11]:
+
+
+# streamlit run webapp.py
+
+
+# In[ ]:
+
+
+
+
